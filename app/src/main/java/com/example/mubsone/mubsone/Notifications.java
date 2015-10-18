e20f41b62f54543fa098813106b90ff067ee4369
brajansaraci <brajansaraci@gmail.com>
2015-10-18 00:28:59 -1200

    Modification of toolbar

--- app/src/main/java/com/example/mubsone/mubsone/Notifications.java
+++ app/src/main/java/com/example/mubsone/mubsone/Notifications.java
@@ -11,6 +11,7 @@ import android.content.Context;
 import android.widget.ListView;
 import android.widget.Toast;
 
+
 public class Notifications extends ListActivity {
     String [] names = {"comment_icon", "description_icon", "see_icon", "separator_icon", "share_icon", "user_icon", "viewer_icon"};
     int [] images = {R.drawable.comment_icon, R.drawable.description_icon, R.drawable.see_icon, R.drawable.separator_icon, R.drawable.share_icon, R.drawable.user_icon, R.drawable.viewer_icon};
@@ -19,7 +20,6 @@ public class Notifications extends ListActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notifications);
-
         //Create and set up adapter
 
         NotificationListAdapter ntfListAdapter = new NotificationListAdapter(this, names, images);
